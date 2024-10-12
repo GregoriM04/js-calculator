8c855dedbef8cb52b3ed518c69f7d14b43e59c49
Gregori Martinez <gregorim143@gmail.com>
2024-10-12 17:41:32 -0400

    Main functionality added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Dark/Light Mode
+/* Dark/Light Mode */
 const body = document.querySelector("body");
 const modeToggle = document.getElementById("icon");
 const modeIcon = document.querySelector(".icon ion-icon");
@@ -26,3 +26,138 @@ function modeChecker() {
     modeIcon.setAttribute("name", "moon-outline");
   }
 }
+
+/* Main functionality */
+const progress = document.getElementById("progress");
+const userInput = document.getElementById("user-input");
+const resetBtn = document.querySelector(".reset");
+const eraserBtn = document.querySelector(".eraser");
+const allInputs = document.querySelectorAll("#numpad-container .input");
+
+// Store expresion and result when calculated
+let completeExpresion = "";
+let previousResult = "";
+
+allInputs.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    let result = "";
+    
+    if (e.target.classList.contains("reset")) {
+      userInput.innerText = 0;
+      progress.innerText = "";
+      completeExpresion = "";
+      previousResult = "";
+    } else if (e.target.classList.contains("eraser")) {
+      userInput.innerText = 0;
+      progress.innerText = progress.innerText.slice(
+        0,
+        progress.innerText.length - 1
+      );
+    } else if (e.target.classList.contains("number")) {
+      if (userInput.innerText == 0) {
+        userInput.innerText = e.target.innerText;
+      } else {
+        userInput.innerText += e.target.innerText;
+      }
+      progress.innerText += e.target.innerText;
+    } else if (e.target.classList.contains("operator")) {
+        if(calculated(completeExpresion)) {
+            progress.innerText = previousResult;
+        }
+        progress.innerText += e.target.innerText;
+        userInput.innerText = 0;
+        completeExpresion = "";
+        previousResult = "";
+    } else if (e.target.classList.contains("equals")) {
+      progress.innerText += e.target.innerText;
+      let readyToCalc = progress.innerText.slice(
+        0,
+        progress.innerText.length - 1
+      );
+
+    //   get the result
+      result += calculate(readyToCalc);
+      
+    //   populate labels with expresion and result
+        progress.innerText += result;
+      userInput.innerText = result;
+
+    //  saving expresion and result
+        completeExpresion += progress.innerText;
+        previousResult += result;
+
+    }
+  });
+});
+
+
+// Function to calculate the user expresion
+function calculate(expression) {
+  //build an array containing the individual parts
+  let parts = expression.match(/(?:\-?[\d\.]+)|[-\+\*\/]|\s+/g);
+
+  //build a separate array containing parsed numbers
+  let nums = parts.map(parseFloat);
+
+  //build another array with all operations reduced to additions
+  let processed = [];
+
+  for (let i = 0; i < parts.length; i++) {
+    if (nums[i] === nums[i]) {
+      //nums[i] isn't NaN
+
+      processed.push(nums[i]);
+    } else {
+      switch (parts[i]) {
+        case "+":
+          continue; //ignore
+
+        case "-":
+          processed.push(nums[++i] * -1);
+
+          break;
+
+        case "*":
+          processed.push(processed.pop() * nums[++i]);
+
+          break;
+
+        case "/":
+          processed.push(processed.pop() / nums[++i]);
+
+          break;
+
+        default:
+          throw new Error("unknown operation: " + parts[i]);
+      }
+    }
+  }
+
+  //add all numbers and return the result
+  return processed.reduce(function (result, elem) {
+    return result + elem;
+  });
+}
+
+// Func to check if the operation has been made so you can do another one with the previous result
+function calculated(str) {
+  let previousCalc = str.split("");
+  let foundSome = "";
+
+  previousCalc.forEach((el) => {
+    if (el == "=") {
+      foundSome += el;
+    }
+  });
+
+  if (foundSome) {
+    return true;
+  }
+  return false;
+}
+
+// let strX = "8*10=80";
+// console.log(calculated(strX));
+
+// let strY = "8*1080";
+// console.log(calculated(strY))
